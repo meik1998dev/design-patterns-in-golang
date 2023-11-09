01a1f56b982e7b26140d9a8f90580624063d0615
meik1998news <mohammad+test@newsifier.com>
2023-11-09 11:20:00 +0300

    test cases for implementing factory pattern

---
+++ factory-method/index_test.go
@@ -0,0 +1,43 @@
+package factorymethod
+
+import (
+	"go-design-patterns/helpers"
+	"testing"
+)
+
+// TestNewCar checks if NewCar returns a Car with the correct initialized name.
+func TestNewCar(t *testing.T) {
+	name := "Speedy"
+	car := NewCar(name)
+	if car.Name() != name {
+		t.Errorf("Expected Car name to be %s, got %s", name, car.Name())
+	}
+}
+
+func TestNewPlane(t *testing.T) {
+	name := "Glider"
+	plane := NewPlane(name)
+	if plane.Name() != name {
+		t.Errorf("Expected Plane name to be %s, got %s", name, plane.Name())
+	}
+}
+
+func TestNewTrain(t *testing.T) {
+	name := "ChooChoo"
+	train := NewTrain(name)
+	if train.Name() != name {
+		t.Errorf("Expected Train name to be %s, got %s", name, train.Name())
+	}
+}
+
+func TestPlanePlay(t *testing.T) {
+	plane := NewPlane("Glider")
+	output := helpers.CaptureOutput(func() {
+		plane.Play()
+	})
+
+	expected := "Flying high!\n" // fmt.Println adds a newline.
+	if output != expected {
+		t.Errorf("Expected Plane Play output to be %q, got %q", expected, output)
+	}
+}
